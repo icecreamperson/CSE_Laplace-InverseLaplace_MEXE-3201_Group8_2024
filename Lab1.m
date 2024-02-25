a7d80e62d016af4ec9cdf603eb38a2573f4192d1
Emanuel Echaluce <157708843+Emanskiee@users.noreply.github.com>
2024-02-25 14:47:35 +0800

    Update Lab1.m

--- Lab1.m
+++ Lab1.m
@@ -21,10 +21,10 @@ i3 = (7/((s^2)+6));
 I3 = ilaplace(i3)
 
 A4 = 1/(s*(s^2+2*s+2));
-a4 = laplace(A4)
+a4 = ilaplace(A4)
 
 A5 = (5*(s+2))/(s^2*(s+1)*(s+3));
-a5 = laplace(A5)
+a5 = ilaplace(A5)
 
 A6 = (s^4+2*s^3+3*s^2+4*s+5)/(s*(s+1));
-a6 = laplace(A6)
+a6 = ilaplace(A6)
